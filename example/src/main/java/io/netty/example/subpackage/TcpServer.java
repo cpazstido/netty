d7046b95319fde696750f55c00b4571855527832
cpazstido <120964417@qq.com>
2018-11-27 14:35:09 +0800

    .

---
+++ example/src/main/java/io/netty/example/subpackage/TcpServer.java
@@ -0,0 +1,78 @@
+package io.netty.example.subpackage;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.util.CharsetUtil;
+import io.netty.util.ReferenceCountUtil;
+
+import java.io.UnsupportedEncodingException;
+
+public class TcpServer {
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch)
+                                throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            // LineBasedFrameDecoder按行分割消息
+                            pipeline.addLast(new LineBasedFrameDecoder(80));
+                            // 再按UTF-8编码转成字符串
+                            pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
+                            pipeline.addLast(new TcpServerHandler());
+                        }
+                    });
+            ChannelFuture f = b.bind(12345).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
+
+class TcpServerHandler extends ChannelInboundHandlerAdapter {
+
+    private int counter = 0;
+    // 接收到新的数据
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws UnsupportedEncodingException {
+        // msg经过StringDecoder后类型不再是ByteBuf而是String
+        String line = (String) msg;
+
+        counter++;
+        System.out.println("第" + counter + "次请求:" + line);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        System.out.println("channelActive");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) {
+        System.out.println("channelInactive");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
